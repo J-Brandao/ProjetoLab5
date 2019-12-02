95f28e90eb1f614d957f0e14f84f6893acfbdf89
J-Brandao <jp.brandao@ua.pt>
2019-12-02 13:48:35 +0000

    LEft-side

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -3,13 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./Components/Navbar";
+import Left_side from "./Components/Left_side";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
 
 function App() {
   return (
     <div>
-
         <NavBar/>
+        <Container >
+        <Row>
+            <Col ><Left_side/></Col>
+
+            <Col><span>Aqui fica a lista</span></Col>
+        </Row>
+        </Container>
     </div>
   );
 }
