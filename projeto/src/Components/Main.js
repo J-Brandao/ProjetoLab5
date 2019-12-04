ef85c17e9d0fb21f81f1d6fab448b3008908174c
João Rangel <Jrangel29@ua.pt>
2019-12-04 11:51:58 +0000

    página de listagem

---
+++ projeto/src/Components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Left from "./Left_side";
+import Right from "./Right_side";
+import '../App.css'
+
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div className={"container-fluid row"}>
+                <div className={"col-6"}>
+                    <Left id={"left-container"}/>
+                </div>
+                <div className={"col-6"}>
+                    <Right id={"right-container"}/>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Main;
